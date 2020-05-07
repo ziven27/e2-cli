b98742788d3cd76410b7f005291f0b100ba30987
27 <zhengchengzhong@yuewen.com>
2020-05-07 13:08:58 +0800

    feat: 让文件名也可以使用ejs

---
+++ example/pages/index/index.js
@@ -0,0 +1,11 @@
+/**
+ * Author: <%= data.authorName %>
+ * Date: <%= data.date %>
+ * Desc: 这是什么？
+ */
+import React from 'react';
+
+function App() {
+    return (<div>hello world</div>);
+}
+export default App;
